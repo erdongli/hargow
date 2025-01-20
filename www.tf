373b76f6dea0705b3b5b2788f043f532c3410c43
Erdong Li <1317183+erdongli@users.noreply.github.com>
2025-01-20 11:13:31 -0800

    add frontendconfig

--- www.tf
+++ www.tf
@@ -2,10 +2,18 @@ resource "google_compute_address" "www" {
   name = "www"
 }
 
+resource "google_artifact_registry_repository" "www" {
+  location      = "us-central1"
+  repository_id = "www"
+  format        = "DOCKER"
+}
+
 resource "kubernetes_namespace" "www" {
   provider = kubernetes.c0
 
   metadata {
+    name = "www"
+
     annotations = {
       name = "www"
     }
@@ -13,7 +21,34 @@ resource "kubernetes_namespace" "www" {
     labels = {
       name = "www"
     }
+  }
+}
 
-    name = "www"
+resource "kubernetes_manifest" "frontend_config_www" {
+  provider = kubernetes.c0
+
+  manifest = {
+    apiVersion = "networking.gke.io/v1beta1"
+    kind       = "FrontendConfig"
+
+    metadata = {
+      name      = "www"
+      namespace = kubernetes_namespace.www.metadata[0].name
+
+      annotations = {
+        name = "www"
+      }
+
+      labels = {
+        name = "www"
+      }
+    }
+
+    spec = {
+      redirectToHttps = {
+        enabled          = true
+        responseCodeName = "301"
+      }
+    }
   }
 }
