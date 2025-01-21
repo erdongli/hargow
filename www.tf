c2f8532088ed7b9ba471289382b79175771e62e1
Erdong Li <1317183+erdongli@users.noreply.github.com>
2025-01-20 16:10:56 -0800

    project-level artifact registry

--- www.tf
+++ www.tf
@@ -2,12 +2,6 @@ resource "google_compute_address" "www" {
   name = "www"
 }
 
-resource "google_artifact_registry_repository" "www" {
-  location      = "us-central1"
-  repository_id = "www"
-  format        = "DOCKER"
-}
-
 resource "kubernetes_namespace" "www" {
   provider = kubernetes.c0
 
