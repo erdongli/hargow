f78ab25c6e54ad65e5490b215e82ac5ee49a5b8d
Erdong Li <1317183+erdongli@users.noreply.github.com>
2025-01-20 21:02:10 -0800

    add service and ingress

--- www.tf
+++ www.tf
@@ -1,40 +1,47 @@
-resource "google_compute_address" "www" {
+locals {
   name = "www"
+
+  domains = [
+    "erdongli.com",
+    "www.erdongli.com",
+  ]
+}
+
+resource "google_compute_address" "www" {
+  name = local.name
 }
 
 resource "kubernetes_namespace" "www" {
   provider = kubernetes.c0
 
   metadata {
-    name = "www"
+    name = local.name
 
     labels = {
-      app = "www"
+      app = local.name
     }
   }
 }
 
-resource "kubernetes_manifest" "frontend_config_www" {
+resource "kubernetes_service" "www" {
   provider = kubernetes.c0
 
-  manifest = {
-    apiVersion = "networking.gke.io/v1beta1"
-    kind       = "FrontendConfig"
+  metadata {
+    name      = local.name
+    namespace = kubernetes_namespace.www.metadata[0].name
+  }
 
-    metadata = {
-      name      = "www"
-      namespace = kubernetes_namespace.www.metadata[0].name
+  spec {
+    type = "NodePort"
 
-      labels = {
-        app = "www"
-      }
+    selector = {
+      app = local.name
     }
 
-    spec = {
-      redirectToHttps = {
-        enabled          = true
-        responseCodeName = "301"
-      }
+    port {
+      port        = 8080
+      target_port = 8080
+      protocol    = "TCP"
     }
   }
 }
@@ -47,16 +54,83 @@ resource "kubernetes_manifest" "managed_certificate_www" {
     kind       = "ManagedCertificate"
 
     metadata = {
-      name      = "www"
+      name      = local.name
       namespace = kubernetes_namespace.www.metadata[0].name
 
       labels = {
-        app = "www"
+        app = local.name
       }
     }
 
     spec = {
-      domains = ["erdongli.com", "www.erdongli.com"]
+      domains = local.domains
+    }
+  }
+}
+
+resource "kubernetes_manifest" "frontend_config_www" {
+  provider = kubernetes.c0
+
+  manifest = {
+    apiVersion = "networking.gke.io/v1beta1"
+    kind       = "FrontendConfig"
+
+    metadata = {
+      name      = local.name
+      namespace = kubernetes_namespace.www.metadata[0].name
+
+      labels = {
+        app = local.name
+      }
+    }
+
+    spec = {
+      redirectToHttps = {
+        enabled          = true
+        responseCodeName = "301"
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "www" {
+  provider = kubernetes.c0
+
+  metadata {
+    name      = local.name
+    namespace = kubernetes_namespace.www.metadata[0].name
+    annotations = {
+      "kubernetes.io/ingress.class"                 = "gce"
+      "kubernetes.io/ingress.global-static-ip-name" = google_compute_address.www.name
+      "networking.gke.io/managed-certificates"      = kubernetes_manifest.managed_certificate_www.manifest.metadata.name
+      "networking.gke.io/v1beta1.FrontendConfig"    = kubernetes_manifest.frontend_config_www.manifest.metadata.name
+    }
+  }
+
+  spec {
+    dynamic "rule" {
+      for_each = local.domains
+
+      content {
+        host = rule.value
+
+        http {
+          path {
+            path      = "/"
+            path_type = "Prefix"
+
+            backend {
+              service {
+                name = kubernetes_service.www.metadata[0].name
+
+                port {
+                  number = kubernetes_service.www.spec[0].port[0].port
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
