bf97b0cf5683787a7e78771fc43995c5b6e1f489
Erdong Li <1317183+erdongli@users.noreply.github.com>
2025-01-26 23:31:32 -0800

    simplify

--- main.tf
+++ main.tf
@@ -1,3 +1,38 @@
+terraform {
+  required_version = "1.10.4"
+
+  backend "gcs" {
+    bucket = "hargow-terraform-backend"
+    prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.16.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.1"
+    }
+  }
+}
+
+provider "google" {
+  project = "hargow"
+  region  = "us-central1"
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  alias                  = "c0"
+  host                   = "https://${module.gke_c0.endpoint}"
+  cluster_ca_certificate = base64decode(module.gke_c0.cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
 resource "google_artifact_registry_repository" "docker" {
   repository_id = "docker"
   format        = "DOCKER"
